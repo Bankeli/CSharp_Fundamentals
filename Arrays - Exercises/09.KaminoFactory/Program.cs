c5a3101315d6bc9e8a93a30e3afb328869814d57
Bankeli <Bankeli995@gmail.com>
2025-08-03 09:25:54 +0300

    Arrays - Exercises

---
+++ Arrays - Exercises/09.KaminoFactory/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _09.KaminoFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dna = int.Parse(Console.ReadLine()); 
+            int[] dnaLenght = new int[dna];
+        
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Clone them!")
+            {
+                dnaLenght = command.Split("!").Select(int.Parse).ToArray();
+            }
+        }
+    }
+}
